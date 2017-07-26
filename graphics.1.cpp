3e336f65512a0353a9c171f8be535b058a47b270
Daniil Andreev <Lolomolo@Daniils-MacBook-Pro.local>
2017-07-26 22:41:54 +0200

    arrays and vec now wrapped in structs

--- graphics.1.cpp
+++ graphics.1.cpp
@@ -1,30 +1,19 @@
 #include <stdio.h>
 #include <math.h>
 
+struct vec3 {
+	int vec[3];
+} v;
+
 struct s3Mat {
 	int arr[3][3];
 } a;
-
-void printMat( int arr[][3], int sizeI, int sizeJ);
 
 int view[2][3] = {{0,0,0},{0,1,0}}; //viewing from (1,1,0) in the direction of (2,2,0) 
 int viewAngle = 180; //degrees
 int line1[2][3] = {{1,1,0},{1,1,1}};
 int resMat[3][3];
 
-
-int matMult(int a[][3], int b[][3], int sizeI, int sizeJ) {
-	//int resMat[sizeI][sizeJ]; //resultant matrix from a and b multiplication
-
-	for(int i = 0; i < sizeI; i++) {
-		for(int j = 0; j < sizeJ; j++) {
-			for(int k = 0; k < sizeI; k++) {
-                resMat[i][j] += a[i][k] * b[k][j];
-			}
-		}
-	} 
-	return 0;
-}
 
 int proj(int v[], int p[]) {	//project p onto v, return prjection vector
 	int projVec;
@@ -42,7 +31,15 @@ void printMat(struct s3Mat a, int sizeI, int sizeJ) {
 	}
 }
 
-s3Mat arrInit(struct s3Mat a) {
+void printVec(struct vec3 v, int sizeI) {
+	for(int i = 0; i < sizeI; i++) {
+		printf("%d", v.vec[i]);
+		printf(" ");
+	}
+	printf("\n"); 
+}
+
+s3Mat matInit(struct s3Mat a) {
 	for(int i = 0; i < 3; i++) {
 		for(int j = 0; j < 3; j++) {
 			a.arr[i][j]=i+2*j;
@@ -51,13 +48,24 @@ s3Mat arrInit(struct s3Mat a) {
 	return a;
 }
 
+vec3 vecInit(struct vec3 v) {
+	for(int i = 0; i < 3; i++) {
+		v.vec[i]=i;
+	}
+	return v;
+}
+
+
 int main() {
 	int height = 3;
 	int width = 3;
 
-	a = arrInit(a);
+	a = matInit(a);
+	v = vecInit(v);
 
 	printMat(a, height, width);
+	printf("\n"); 
+	printVec(v, height);
 	
 	return 0;
 }
