87d8d37af0702c791ca0ec59d1b0ea4241fd2eeb
Daniil Andreev <Lolomolo@Daniils-MacBook-Pro.local>
2017-07-27 14:14:57 +0200

    array, line, mat, & vec work, as well as line length

--- graphics.1.cpp
+++ graphics.1.cpp
@@ -5,20 +5,21 @@ struct vec3 {
 	int vec[3];
 } v;
 
+struct line {
+	int pS[3];
+	int pE[3];
+} l;
+
 struct s3Mat {
 	int arr[3][3];
 } a, b;
 
+/*
 int view[2][3] = {{0,0,0},{0,1,0}}; //viewing from (1,1,0) in the direction of (2,2,0) 
 int viewAngle = 180; //degrees
 int line1[2][3] = {{1,1,0},{1,1,1}};
 int resMat[3][3];
-
-
-int proj(int v[], int p[]) {	//project p onto v, return prjection vector
-	int projVec;
-	return projVec;
-}
+*/
 
 void printMat(struct s3Mat a, int sizeI, int sizeJ) {
 	for(int i = 0; i < sizeI; i++) {
@@ -31,9 +32,22 @@ void printMat(struct s3Mat a, int sizeI, int sizeJ) {
 	}
 }
 
-void printVec(struct vec3 v, int sizeI) {
-	for(int i = 0; i < sizeI; i++) {
+void printVec(struct vec3 v, int length) {
+	for(int i = 0; i < length; i++) {
 		printf("%d", v.vec[i]);
+		printf(" ");
+	}
+	printf("\n"); 
+}
+
+void printLine(struct line l, int length) {
+	for(int i = 0; i < length; i++) {
+		printf("%d", l.pS[i]);
+		printf(" ");
+	}
+	printf(" ");
+	for(int j = 0; j < length; j++) {
+		printf("%d", l.pE[j]);
 		printf(" ");
 	}
 	printf("\n"); 
@@ -55,6 +69,16 @@ vec3 vecInit(struct vec3 v) {
 	return v;
 }
 
+line lineInit(struct line l) {
+	for(int i = 0; i < 3; i++) {
+		l.pS[i]=i;
+	}
+	for(int j = 0; j < 3; j++) {
+		l.pE[j]=j+3;
+	}
+	return l;
+}
+
 s3Mat matMult(struct s3Mat a, struct s3Mat b) {
 	s3Mat c;
 
@@ -68,10 +92,31 @@ s3Mat matMult(struct s3Mat a, struct s3Mat b) {
 	return c;
 }
 
+double lineLength(struct line l) {
+	vec3 temp;
+	double ret = 0;
+	for(int i = 0; i < 3; i++) {
+		temp.vec[i] = l.pE[i] - l.pS[i];	//turn line into vec that origionates at the origin
+	}
+	for(int i = 0; i < 3; i++) {
+		ret += temp.vec[i]*temp.vec[i];
+	}
+	return(sqrt(ret));
+}
+/*
+line proj(line p, line v) {	//project p onto v, return prjection vector
+	//if(p.pS!=v.pS) { }  //make some error shit if the starting point of the two lines are different
+	line retLine;
+	retLine = lineInit(retLine);
+
+}
+*/
+
 int main() {
 	int height = 3;
 	int width = 3;
 
+/*
 	a = matInit(a);
 	b = matInit(b);
 
@@ -81,6 +126,16 @@ int main() {
 	printf("\n"); 
 	printMat(s, height, width);
 	printf("\n");
+*/
+
 	
+
+	l = lineInit(l);
+
+	printLine(l,3);
+
+	printf("%f", lineLength(l));
+	printf("\n");
+
 	return 0;
 }
